5af37e2e8a20cff55cd4e9205565e94650deef43
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-12-31 13:21:22 +0200

    Initial Commit

---
+++ AudioRecorderInSwiftUI/AudioRecorderInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderInSwiftUIApp.swift
+//  AudioRecorderInSwiftUI
+//
+//  Created by Ramill Ibragimov on 31.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
